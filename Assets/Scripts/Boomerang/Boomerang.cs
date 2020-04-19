7fd6ebfd6480ca7703a48674c1848426ed5045f4
artisiole <43116512+artisiole@users.noreply.github.com>
2020-04-19 14:08:03 -0500

    started work on the boomerang, not finished though

---
+++ Assets/Scripts/Boomerang/Boomerang.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomerang : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public BoxCollider2D bc;
+    public GameObject br;
+
+    Vector2 vforce;
+    Vector2 brakingforce;
+
+    void Start()
+    {
+        Throw(600, new Vector2(0,0));
+    }
+
+    void Throw(float force, Vector2 direction)
+    {
+        bc.enabled = false;
+        vforce.x = force;
+        vforce.y = 0;
+        brakingforce.x = -force/10;
+        brakingforce.y = 0;
+        rb.AddRelativeForce(vforce);
+        StartCoroutine(brake());
+        
+    }
+
+    IEnumerator brake()
+    {
+        yield return new WaitForSeconds(0.5f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        bc.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce * 2);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce * 2);
+        yield return new WaitForSeconds(0.1f);
+        rb.AddRelativeForce(brakingforce * 2);
+        yield return new WaitForSeconds(10f);
+        Destroy(br);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBoomerang.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoomerang : MonoBehaviour
+{
+    public GameObject boomerang;
+    public bool activated = true;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (activated)
+            {
+                Vector3 targetDirection = new Vector3(Input.mousePosition.x, Input.mousePosition.x, 0);
+                Instantiate(boomerang, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+                activated = false;
+                StartCoroutine(cooldown());
+            }
+        }
+        
+    }
+
+    IEnumerator cooldown()
+    {
+        yield return new WaitForSeconds(0.0f);
+        activated = true;
+    }
+}
