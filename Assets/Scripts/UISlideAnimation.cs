03d71be7680b6e9ede0ba2c3e0a28ba1e73c5c6f
DockFrankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2020-04-19 19:31:33 +0200

    Added a basic menu UI and simple animations, will probably change

---
+++ Assets/Scripts/UISlideAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISlideAnimation : MonoBehaviour
+{
+    public Vector2 position = new Vector2(0.5f, 0.5f);
+
+    private RectTransform trans;
+
+    private void Awake()
+    {
+        Assign();
+        Update();
+    }
+
+    private void Assign()
+    {
+        trans = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        FitToScreen();
+        NewPosition();
+    }
+
+    private void NewPosition()
+    {
+        Vector2 nPosition = new Vector2(Screen.width, Screen.height) * position;
+
+        trans.position = nPosition;
+    }
+
+    private void FitToScreen()
+    {
+        trans.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+}
