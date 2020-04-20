04363dd88923d676b5e88f941ed5bb40f4828505
DockFrankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2020-04-20 21:10:01 +0200

    Menu changes

---
+++ Assets/Scripts/videoSkip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class videoSkip : MonoBehaviour
+{
+    public string sceneToLoad = "";
+    public float videoLenght;
+
+    private void Awake()
+    {
+        StartCoroutine(waitForVideo());
+    }
+
+    private IEnumerator waitForVideo()
+    {
+        yield return new WaitForSeconds(videoLenght + 0.5f);
+        LoadScene();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
