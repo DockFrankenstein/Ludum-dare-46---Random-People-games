c4e5e79faf04e28560c3a3a1386d988668f7769f
KubikZ <62077663+KubikZ@users.noreply.github.com>
2020-04-20 21:14:00 +0200

    lolz

--- Assets/Scripts/LevelManagment/LevelLoadTrigger.cs
+++ Assets/Scripts/LevelManagment/LevelLoadTrigger.cs
@@ -5,7 +5,7 @@ class LevelLoadTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-            LevelManager.LoadNextLevel();
+            LevelManager.current.LoadNextLevel();
     }
 
 }
