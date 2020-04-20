a183d1d67e3a0dcd2993cf9b0a95a6056c138ed4
artisiole <43116512+artisiole@users.noreply.github.com>
2020-04-19 23:09:23 -0500

    minor bugfix i immediatley forgot about after merging

--- Assets/Scripts/Breakable Blocks/BreakableBlock.cs
+++ Assets/Scripts/Breakable Blocks/BreakableBlock.cs
@@ -11,10 +11,10 @@ public class BreakableBlock : MonoBehaviour
     {
         if (col.collider.tag == "Boomerang")
         {
-            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), transform.position));
-            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), transform.position));
-            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), transform.position));
-            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), transform.position));
+            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.Identity, transform.position));
+            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.Identity, transform.position));
+            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.Identity, transform.position));
+            Instantiate(debris, new Vector2(transform.position.x + Random.Range(-0.7f, 0.7f), transform.position.y + Random.Range(-0.7f, 0.7f)), Quaternion.Identity, transform.position));
             Destroy(block);
         }
     }
